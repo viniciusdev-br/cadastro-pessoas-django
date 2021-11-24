cc8c2009bdcea6bd9a679b600c11e6f2e12757c2
Vinius Botelho <viniciusvcb2002@gmail.com>
2021-11-23 23:08:53 -0300

    Variaveis de ambiente criadas

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^7!64v@rl8qzh)l)el(+653)g!3!3ain9rj2t&0g9_mm%*2e4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.getenv('DEBUG', False)
 ALLOWED_HOSTS = []
 
 
